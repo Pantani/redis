66acdc3231ebc490f7d5eadd1be8321b1fb6c877
Danilo Pantani <danpantani@gmail.com>
2020-09-15 22:38:43 -0300

    Update lib and use contexts

--- hmap.go
+++ hmap.go
@@ -1,58 +1,60 @@
 package redis
 
 import (
+	"context"
 	"encoding/json"
+
 	"github.com/Pantani/errors"
 )
 
 // GetAllHMObjects get all objects from a hash map table.
 // It returns the objects and an error if occurs.
-func (db *Redis) GetAllHMObjects(entity string) (map[string]string, error) {
-	cmd := db.client.HGetAll(entity)
-	if cmd.Err() != nil {
-		return nil, errors.E("not found", cmd.Err())
+func (db *Redis) GetAllHMObjects(ctx context.Context, entity string) (map[string]string, error) {
+	cmd := db.client.HGetAll(ctx, entity)
+	if err := cmd.Err(); err != nil {
+		return nil, errors.E("not found", err, errors.Params{"entity": entity})
 	}
 	return cmd.Val(), nil
 }
 
 // GetHMObject get an object from a hash map table.
 // It returns an error if occurs.
-func (db *Redis) GetHMObject(entity, key string, value interface{}) error {
-	cmd := db.client.HMGet(entity, key)
-	if cmd.Err() != nil {
-		return errors.E("not found", cmd.Err(), errors.Params{"key": key})
+func (db *Redis) GetHMObject(ctx context.Context, entity, key string, value interface{}) error {
+	cmd := db.client.HMGet(ctx, entity, key)
+	if err := cmd.Err(); err != nil {
+		return errors.E("not found", err, errors.Params{"entity": entity, "key": key})
 	}
 	val, ok := cmd.Val()[0].(string)
 	if !ok {
-		return errors.E("not found")
+		return errors.E("not found", errors.Params{"entity": entity, "key": key, "value": val})
 	}
 	err := json.Unmarshal([]byte(val), value)
 	if err != nil {
-		return errors.E("not found", err, errors.Params{"key": key})
+		return errors.E("fail to unmarshal value", err, errors.Params{"entity": entity, "key": key, "value": val})
 	}
 	return nil
 }
 
 // AddHMObject add an object to a hash map table.
 // It returns an error if occurs.
-func (db *Redis) AddHMObject(entity, key string, value interface{}) error {
+func (db *Redis) AddHMObject(ctx context.Context, entity, key string, value interface{}) error {
 	j, err := json.Marshal(value)
 	if err != nil {
 		return errors.E(err, errors.Params{"key": key})
 	}
-	cmd := db.client.HMSet(entity, map[string]interface{}{key: j})
-	if cmd.Err() != nil {
-		return errors.E("not stored", cmd.Err(), errors.Params{"key": key})
+	cmd := db.client.HMSet(ctx, entity, map[string]interface{}{key: j})
+	if err := cmd.Err(); err != nil {
+		return errors.E("not stored", err, errors.Params{"entity": entity, "key": key})
 	}
 	return nil
 }
 
 // DeleteHMObject delete an object from a hash map table.
 // It returns an error if occurs.
-func (db *Redis) DeleteHMObject(entity, key string) error {
-	cmd := db.client.HDel(entity, key)
-	if cmd.Err() != nil {
-		return errors.E("not deleted", cmd.Err(), errors.Params{"key": key})
+func (db *Redis) DeleteHMObject(ctx context.Context, entity, key string) error {
+	cmd := db.client.HDel(ctx, entity, key)
+	if err := cmd.Err(); err != nil {
+		return errors.E("not deleted", err, errors.Params{"entity": entity, "key": key})
 	}
 	return nil
 }
--- redis.go
+++ redis.go
@@ -1,13 +1,16 @@
 package redis
 
 import (
+	"context"
 	"encoding/json"
+
 	"github.com/Pantani/errors"
 	"github.com/go-redis/redis"
 )
 
 type Redis struct {
-	client *redis.Client
+	client  *redis.Client
+	context *context.Context
 }
 
 // Init initialize the database passing the host url.
@@ -18,7 +21,7 @@ func (db *Redis) Init(host string) error {
 		return errors.E(err, "Cannot connect to Redis")
 	}
 	client := redis.NewClient(options)
-	if err := client.Ping().Err(); err != nil {
+	if err := client.Ping(context.Background()).Err(); err != nil {
 		return errors.E(err, "Redis connection test failed")
 	}
 	db.client = client
@@ -27,47 +30,48 @@ func (db *Redis) Init(host string) error {
 
 // GetObject get object from key.
 // It returns an error if occurs.
-func (db *Redis) GetObject(key string, value interface{}) error {
-	cmd := db.client.Get(key)
-	if cmd.Err() != nil {
-		return errors.E("not found", cmd.Err(), errors.Params{"key": key})
-	}
-	err := json.Unmarshal([]byte(cmd.Val()), value)
-	if err != nil {
+func (db *Redis) GetObject(ctx context.Context, key string, value interface{}) error {
+	cmd := db.client.Get(ctx, key)
+	if err := cmd.Err(); err != nil {
 		return errors.E("not found", err, errors.Params{"key": key})
+	}
+	val := cmd.Val()
+	err := json.Unmarshal([]byte(val), value)
+	if err != nil {
+		return errors.E("fail to unmarshal value", err, errors.Params{"key": key, "value": val})
 	}
 	return nil
 }
 
 // AddObject add object for a key.
 // It returns an error if occurs.
-func (db *Redis) AddObject(key string, value interface{}) error {
+func (db *Redis) AddObject(ctx context.Context, key string, value interface{}) error {
 	j, err := json.Marshal(value)
 	if err != nil {
-		return errors.E(err, errors.Params{"key": key})
+		return errors.E("fail to marshal value", err, errors.Params{"key": key})
 	}
-	cmd := db.client.Set(key, j, 0)
-	if cmd.Err() != nil {
-		return errors.E("not stored", cmd.Err(), errors.Params{"key": key})
+	cmd := db.client.Set(ctx, key, j, 0)
+	if err := cmd.Err(); err != nil {
+		return errors.E("not stored", err, errors.Params{"key": key})
 	}
 	return nil
 }
 
 // DeleteObject delete object from key.
 // It returns an error if occurs.
-func (db *Redis) DeleteObject(key string) error {
-	cmd := db.client.Del(key)
-	if cmd.Err() != nil {
-		return errors.E("not deleted", cmd.Err(), errors.Params{"key": key})
+func (db *Redis) DeleteObject(ctx context.Context, key string) error {
+	cmd := db.client.Del(ctx, key)
+	if err := cmd.Err(); err != nil {
+		return errors.E("not deleted", err, errors.Params{"key": key})
 	}
 	return nil
 }
 
 // IsReady verify the database is ready.
 // It returns true if ready.
-func (db *Redis) IsReady() bool {
+func (db *Redis) IsReady(ctx context.Context) bool {
 	if db.client == nil {
 		return false
 	}
-	return db.client.Ping().Err() == nil
+	return db.client.Ping(ctx).Err() == nil
 }
